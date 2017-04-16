2fe32eb95f458a763a549a3a9ff748041e0293e7
Jerome PREVOTAT <jprevota@e1r3p21.42.fr>
2017-04-16 15:36:37 +0200

    minor fixes

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jprevota <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/16 14:59:53 by jprevota          #+#    #+#             */
-/*   Updated: 2017/04/14 16:31:53 by jprevota         ###   ########.fr       */
+/*   Updated: 2017/04/16 15:35:08 by jprevota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int		get_next_line(const int fd, char **line)
 	int			i;
 
 	i = 0;
-	if (fd < 0)
+	if (fd < 0 || !line)
 		return (-1);
 	if (buff_end == NULL &&
 			!(buff_end = (char *)malloc(BUFF_SIZE + 1 * sizeof(char))))
@@ -36,8 +36,8 @@ int		get_next_line(const int fd, char **line)
 	}
 	else
 		*line = read_till_nl(fd, buff_end, line);
-	buff_end = ft_strchr(buff_end, '\n') + 1;
-	ft_putendl("A");
+	if (ft_strlen(buff_end) > 0 && check_nl(buff_end) == 1)
+		buff_end = ft_strchr(buff_end, '\n') + 1;
 	if (check_eof(fd, buff_end) == 1)
 		return (0);
 	return (1);
@@ -49,27 +49,19 @@ int		check_eof(int fd, char *buff_end)
 	int		ret;
 	char	*tmp;
 
-	ft_putendl("B");
 	i = 0;
-	while (i < (int)ft_strlen(buff_end))
+	while (buff_end[i] != '\0')
 	{
 		if (buff_end[i] > 0)
-		{
-			ft_putendl("Exiting check eof with 0");
 			return (0);
-		}
 		i++;
 	}
-	ft_putendl("C");
 	tmp = ft_strnew((size_t)(BUFF_SIZE + 1));
 	ret = read(fd, tmp, BUFF_SIZE);
 	tmp[ret] = '\0';
 	if (ret == 0)
 		return (1);
-	else
-		buff_end = ft_strjoin(buff_end, tmp);
-	ft_putstr("strlen buffend : ");
-	ft_putnbr(ft_strlen(buff_end));
+	buff_end = ft_strjoin(buff_end, tmp);
 	return (0);
 }
 
@@ -93,7 +85,6 @@ char	*read_till_nl(int fd, char *buff, char **line)
 	int		ret;
 	char	*tmp;
 
-	ft_putendl("Entering read_till_nl");
 	tmp = ft_strnew((size_t)(BUFF_SIZE + 1));
 	while (check_nl(buff) != 1 && ret > 0)
 	{
@@ -101,23 +92,14 @@ char	*read_till_nl(int fd, char *buff, char **line)
 		ret = read(fd, buff, BUFF_SIZE);
 		buff[ret] = '\0';
 	}
-	ft_putendl("While ended");
 	i = 0;
-	ft_putnbr(check_nl(buff));
-	ft_putstr(buff);
-	while (buff[i] != '\n')
+	while (buff[i] != '\n' && buff[i] != '\0')
 	{
-		//ft_putchar(buff[i]);
 		tmp[i] = buff[i];
 		i++;
 	}
-	if (tmp[0] == '\0')
-		ft_putnbr(1);
-	ft_putendl("While ended");
 	tmp[i] = '\0';
-	ft_putendl("eos added");
 	*line = ft_strjoin(*line, tmp);
-	ft_putendl("Exiting read_till_nl");
 	return (*line);
 }
 
@@ -137,11 +119,12 @@ int		main(int argc, char **argv)
 		while ((gnl = get_next_line(fd, line)) != 0)
 		{
 			ft_putnbr(gnl);
-			ft_putendl("Line :");
+			ft_putendl(" // Line :");
 			ft_putstr(*line);
 			ft_putchar('\n');
 		}
 		ft_putnbr(gnl);
+		ft_putendl(" // Line :");
 		ft_putstr(*line);
 	}
 	else
