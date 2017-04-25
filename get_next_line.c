3eb6cc4fb6214d87e81241247e45a47920343a18
JeJ <jer.prevotat@gmail.com>
2017-04-26 01:16:12 +0200

    major fix concat with memcpy and memore management

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jprevota <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/16 14:59:53 by jprevota          #+#    #+#             */
-/*   Updated: 2017/04/24 00:50:51 by admin            ###   ########.fr       */
+/*   Updated: 2017/04/24 18:02:45 by admin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,24 +26,27 @@ int		get_next_line(const int fd, char **line)
 		return (-1);
 	if (!(*line = (char *)malloc(BUFF_SIZE + 1 * sizeof(char))))
 		return (-1);
-	if (buff_end == NULL &&
-			!(buff_end = (char *)malloc((2 * BUFF_SIZE) + 1 * sizeof(char))))
-		return (-1);
-	ft_strclr(*line);
+	ft_memset(*line, '\0', BUFF_SIZE + 1);
+	if (buff_end == NULL)
+	{
+		if (!(buff_end = (char *)malloc(BUFF_SIZE + 1 * sizeof(char))))
+			return (-1);
+		ft_memset(buff_end, '\0', BUFF_SIZE + 1);
+	}
 	i = 0;
 	if (check_nl(buff_end) == 1)
 	{
 		while (buff_end[i] != '\n')
 			i++;
-		*line = strncpy(*line, buff_end, i);
+		*line = ft_strncpy(*line, buff_end, i);
 	}
 	else
 		read_till_nl(fd, buff_end, line);
 	if (ft_strlen(buff_end) > 0 && check_nl(buff_end) == 1)
 		buff_end = ft_strchr(buff_end, '\n') + 1;
-	ret = fill_buffer(fd, buff_end);
-	if (ret == 0 && (check_nl(buff_end) == 0) && ft_strlen(buff_end) == 0)
-		return (0);
+	if (ft_strlen(buff_end) == 0)
+		if ((ret = fill_buffer(fd, buff_end) == 0) && check_nl(buff_end) == 0)
+			return (0);
 	return (1);
 }
 
@@ -55,20 +58,21 @@ int		fill_buffer(int fd, char *buff_end)
 	tmp = ft_strnew((size_t)(BUFF_SIZE + 1));
 	ret = read(fd, tmp, BUFF_SIZE);
 	tmp[ret] = '\0';
-	buff_end = ft_strncat(buff_end, tmp, ft_strlen(tmp));
+	buff_end = ft_strcat(buff_end, tmp);
 	return (ret);
 }
 
 int		read_till_nl(int fd, char *buff_end, char **line)
 {
-	int		i;
-	int		ret;
+	int	i;
+	int	ret;
 	char	*tmp;
+	char *tmp2;
 
 	tmp = ft_strnew((size_t)(BUFF_SIZE + 1));
 	while (check_nl(buff_end) == 0)
 	{
-		*line = ft_strjoin(*line, buff_end);
+		*line = ft_strjoin(*line, buff_end);		
 		ret = read(fd, buff_end, BUFF_SIZE);
 		buff_end[ret] = '\0';
 	}
@@ -79,7 +83,14 @@ int		read_till_nl(int fd, char *buff_end, char **line)
 		i++;
 	}
 	tmp[i] = '\0';
-	*line = ft_strncat(*line, tmp, ft_strlen(tmp));
+	//Proper concat == ft_strjoin(*line, tmp)
+	tmp2 = (char *)malloc(ft_strlen(*line) + ft_strlen(tmp) + 1);
+	ft_memset(tmp2, '\0', ft_strlen(*line) + ft_strlen(tmp) + 1);
+	ft_memcpy(tmp2, *line, ft_strlen(*line));
+	ft_memcpy(tmp2 + ft_strlen(*line), tmp, ft_strlen(tmp));
+	tmp2[ft_strlen(*line) + ft_strlen(tmp)] = '\0';
+	*line = tmp2;
+	//End concat // free ?
 	return (ret);
 }
 
@@ -107,7 +118,6 @@ int		main(int argc, char **argv)
 	{
 		if ((fd = open(argv[1], O_RDONLY)) == -1)
 			return (-1);
-
 		while ((gnl = get_next_line(fd, &line)) != 0)
 		{
 			ft_putnbr(gnl);
