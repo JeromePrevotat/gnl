8bd9c680ed55a6bb62aef8a4abb607d8b0fe1de7
JeJ <jer.prevotat@gmail.com>
2017-10-18 12:19:10 +0200

    fu

--- get_next_line.c
+++ get_next_line.c
@@ -13,7 +13,7 @@
 #include "./get_next_line.h"
 #include <stdio.h>
 
-static int	check_nl(char *str)
+static int	find_nl(char *str)
 {
 	size_t	i;
 
@@ -24,70 +24,45 @@ static int	check_nl(char *str)
 			return (i);
 		i++;
 	}
-	return (-1);
+	return (i);
 }
 
-static char	*str_catfree(char *line, char *buff, int reset)
+static char	*set_buff_end(char *buff_end)
 {
-	char	*tmp;
-	size_t	size;
-
-	size = ft_strlen(line) + ft_strlen(buff) + 1;
-	if (!(tmp = (char *)malloc(size * sizeof(char))))
-		return (NULL);
-	ft_memset(tmp, '\0', size);
-	ft_memcpy(tmp, line, ft_strlen(line));
-	ft_memcpy(tmp + ft_strlen(tmp), buff, ft_strlen(buff));
-	free(line);
-	if (reset == 1)
-		ft_memset(buff, '\0', ft_strlen(buff));
-	return (tmp);
-}
-
-static int	gnl(char **line, const int fd, int ret)
-{
-	static char	*buff_end = "";
-	char		*buff;
-	char		*tmp;
-	int			i;
-
-	buff_end = (ft_strcmp(buff_end, "") == 0) ? ft_strnew(0) : buff_end;
-	if ((i = check_nl(buff_end)) >= 0)
+	if (ft_strchr(buff_end, '\n'))
 	{
-		*line = ft_strsub(buff_end, 0, i);
-		ft_strcpy(buff_end, buff_end + i + 1);
-		return (1);
+		ft_strcpy(buff_end, buff_end + find_nl(buff_end) + 1);
+		return (buff_end);
 	}
-	*line = ft_strdup(buff_end);
-	free(buff_end);
-	if (!(buff = (char *)malloc(BUFF_SIZE + 1 * sizeof(char))))
-		return (-1);
-	ft_memset(buff, '\0', BUFF_SIZE + 1);
-	while (ret > 0)
+	if (ft_strlen(buff_end) > 0)
 	{
-		if ((ret = read(fd, buff, BUFF_SIZE)) == -1)
-			return (-1);
-		if ((i = check_nl(buff)) >= 0)
-		{
-			tmp = ft_strsub(buff, 0, i);
-			*line = str_catfree(*line, tmp, 0);
-			free(tmp);
-			buff_end = ft_strsub(buff, i + 1, ft_strlen(buff));
-			return (1);
-		}
-		*line = str_catfree(*line, buff, 1);
+		ft_memset(buff_end, '\0', ft_strlen(buff_end));
+		return (buff_end);
 	}
-	*line = str_catfree(buff_end, buff, 1);
-	ft_memset(buff_end, '\0', ft_strlen(buff_end));
-	return (ft_strlen(*line) == 0 ? 0 : 1);
+	return (NULL);
 }
 
 int			get_next_line(const int fd, char **line)
 {
-	int		ret;
+	static char	*buff_end = "";
+	char		buff[BUFF_SIZE + 1];
+	char		*tmp;
+	int			ret;
 
-	ret = 1;
-	if (!line || fd < 0 || BUFF_SIZE <= 0)
+	if (fd < 0 || BUFF_SIZE < 1 || !line || read(fd, buff, 0) < 0)
 		return (-1);
-	return (gnl(line, fd, ret));
+	buff_end = (ft_strcmp("", buff_end) == 0) ? ft_strnew(0) : buff_end;
+	while (ft_strchr(buff_end, '\n') == NULL
+		&& (ret = read(fd, buff, BUFF_SIZE) > 0))
+	{
+		buff[ret] = '\0';
+		printf("BUFFEND : >%s<\nRET : %d\n", buff, ret);
+		tmp = buff_end;
+		buff_end = ft_strjoin(tmp, buff);
+		//free(tmp);
+	}
+	*line = ft_strsub(buff_end, 0, find_nl(buff_end));
+	if (set_buff_end(buff_end) == NULL)
+		return (0);
+	return (1);
 }
